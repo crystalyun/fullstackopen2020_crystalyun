1df588cfdcdeaeaa021dd8d3081f538e2c5fabde
Crystal Yun <crystal@internal.rpahero.com>
2020-06-22 20:53:21 +1000

    exercise 2.15

--- PART2/phonebook/src/App.js
+++ PART2/phonebook/src/App.js
@@ -65,10 +65,14 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
-    setNewName('')
-    setNewNumber('')
   }
 
   const handleNameChange= (event) => {
