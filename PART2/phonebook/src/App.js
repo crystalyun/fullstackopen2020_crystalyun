abc47dd7578a759679370b46848325c75ee43754
Crystal Yun <crystal@internal.rpahero.com>
2020-06-26 03:11:18 +1000

    exercise 2.20

--- PART2/phonebook/src/App.js
+++ PART2/phonebook/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import phonebookService from './services/phonebook'
 
-
-
 const PersonForm = ({ newName, newNumber, addNewPerson, handleNameChange, handleNumberChange }) => {
   return (
     <form onSubmit={addNewPerson}>
@@ -48,13 +46,26 @@ const Persons = ({persons, handleClick}) => {
   )
 }
 
+const Notification = ({ message, isError }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={isError? "errorNotification" : "successfulNotification"}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterName, setFilterName ] = useState('')
-
   const [ persons, setPersons ] = useState([])
-  
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [isError, setIsError] = useState(false)
+
   // ONLY RUNS ONCE 
   useEffect(() => {
     console.log('effect')
@@ -65,7 +76,7 @@ const App = () => {
       })
   },[])
 
-  console.log('rendered', persons.length, 'persons')
+  console.log('rendered App')
 
   const addNewPerson = (event) => {
     event.preventDefault()
@@ -80,6 +91,24 @@ const App = () => {
         .update(registeredPerson.id, updatedRegisteredPerson)
         .then(returnedPerson => {
           setPersons(persons.map(person => person.id !== returnedPerson.id ? person : updatedRegisteredPerson))
+          setNotificationMessage(`Changed number of ${returnedPerson.name}`)
+
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 3000)
+        })
+        .catch(error => {
+          setIsError(true)
+
+          setNotificationMessage(`Information of ${registeredPerson.name} has already been removed from server`)
+
+          setPersons(persons.filter(person => person.name !== registeredPerson.name))
+
+          setTimeout(() => {
+            setNotificationMessage(null);
+            setIsError(false)
+          }, 3000)
+
       })
       }
     } else {
@@ -91,6 +120,11 @@ const App = () => {
         .create(personObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+          setNotificationMessage(`Added ${returnedPerson.name}`)
+
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 3000)
         })
     }
     setNewName('')
@@ -115,6 +149,11 @@ const App = () => {
         .deleteID(id)
         .then(() => {
           setPersons(persons.filter(person => id !== person.id))
+          setNotificationMessage(`Deleted ${name}`)
+
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 3000)
         })
     } else {console.log('no state update')}
   }
@@ -124,6 +163,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={notificationMessage} isError={isError}/>
 
       <Filter filterName={filterName} handleFilterNameChange={handleFilterNameChange} />
       
