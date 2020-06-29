7663f4659113de68e384a711ab260b2786d13423
Crystal Yun <crystal@internal.rpahero.com>
2020-06-30 06:48:48 +1000

    exercise 3.c

--- PART3/phonebook_backend/index.js
+++ PART3/phonebook_backend/index.js
@@ -1,37 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 // const cors = require('cors')
 const app = express()
-
-
-let persons = [
-    { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-    },
-    { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-    },
-    { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-    },
-    { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-    }
-]
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+const Person = require('./models/person')
 
 // app.use(cors())
 app.use(express.static('build'))
@@ -49,32 +21,36 @@ morgan.token('requestBody', function(req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody'));
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(allPeople => {
+        response.json(allPeople)
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`)
+    Person.find({}).then(allPeople => {
+        response.send(`<div>Phonebook has info for ${allPeople.length} people</div><div>${new Date()}</div>`)    
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(person => {
+          console.log('person : ', person)
+          if (person) {
+              response.json(person)
+          } else {
+              response.status(404).end()
+          }
+      })
+      .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log('before delete : ', persons)
-    persons = persons.filter(person => person.id !== id)
-    console.log('after delete : ', persons)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+          response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -100,18 +76,60 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: getRandomInt(10,1000)
-    }
+    })
+
+    // save to DB 
+    person.save().then(savedPerson => {
+        console.log('savedPerson is : ', savedPerson)
+        console.log('type : ', typeof savedPerson)
+        response.json(savedPerson)
+    })
     
-    persons = persons.concat(person)
-    response.json(person)
 })
 
-// `process.env.PORT` allows Heroku (prod environment) to set its PORT on its own. 3001 is for development
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body 
+
+    // can only update number, but not name 
+    const person = {
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+          console.log('updatedPerson : ', updatedPerson)
+          if (updatedPerson) {
+              response.json(updatedPerson)
+          } else {
+              response.status(404).end()
+          }
+        //   console.log('updatedPerson.toJSON(): ', updatedPerson.toJSON())
+        //   console.log('typeof updatedPerson.toJSON(): ', typeof updatedPerson.toJSON()) // object
+
+          // response.json(updatedPerson.toJSON()) also works, but it's redundant as response.json under the hood calls `JSON.stringify`, which in turn calls mongoose method `.toJSON` on the target object to perform any transformations (such as deleting ._id field) before returning JSON data to the client
+      })
+      .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log('middleware error handler')
+    console.log('message : ' , error.message)
+    console.log('name : ' , error.name)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+// `process.env.PORT` allows Heroku (prod environment) to set its PORT on its own. 3001 is reserved for development
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
