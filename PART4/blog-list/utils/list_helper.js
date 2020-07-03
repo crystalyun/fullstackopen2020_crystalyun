b174a03956d0835f49a50158194889297223115f
Crystal Yun <crystal@internal.rpahero.com>
2020-07-04 03:03:46 +1000

    exercise 4.a all

---
+++ PART4/blog-list/utils/list_helper.js
@@ -0,0 +1,53 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  }
+  const favorite = blogs.reduce(reducer)
+  return (({ title, author, likes }) => ({ title, author, likes }))(favorite)
+}
+
+const mostBlogs = (blogs) => {
+  const top = _.chain(blogs)
+    .countBy('author')
+    .map((value, key) => ({'author': key, 'blogs': value}))
+    .maxBy('blogs')
+    .value()
+  
+  return top 
+}
+
+// https://dustinpfister.github.io/2018/11/15/lodash_sum/
+const mostLikes = (blogs) => {
+  const top = _.chain(blogs)
+    .groupBy('author')
+    .map(( objs, key ) => {
+      return {
+        'author': key,
+        'likes' : _.sumBy(objs, 'likes')
+      }
+    })
+    .maxBy('likes')
+    .value()
+
+  return top
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
