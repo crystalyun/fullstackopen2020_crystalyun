91db56077263d710eb6b003e36acb0c9ae1ef585
Crystal Yun <crystal@internal.rpahero.com>
2020-07-08 09:19:04 +1000

    exercise 4.18

--- PART4/blog-list/controllers/blogs.js
+++ PART4/blog-list/controllers/blogs.js
@@ -1,19 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // base binding will be /api/blogs
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { blogs: 0 })
   response.json(blogs.map(blog => blog.toJSON()))
   //response.json serializes `blogs`, an arry of objects (WHICH IS, IN JAVASCRIPT, AN OBJECT), to JSON. calls JSON.stringify under the hood.
   // JSON.stringify then calls .toJSON attached to Blog model which will remove ._id and .__v fields
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    ...request.body,
+    user: user._id
+  })
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog.toJSON())
 })
 
