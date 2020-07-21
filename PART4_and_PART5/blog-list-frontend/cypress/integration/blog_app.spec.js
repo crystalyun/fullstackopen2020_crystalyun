9e88d49ac5fbbc755907e9c161ebb34039751e0f
Crystal Yun <crystal@internal.rpahero.com>
2020-07-21 15:59:39 +1000

    exercise 5.18

--- PART4_and_PART5/blog-list-frontend/cypress/integration/blog_app.spec.js
+++ PART4_and_PART5/blog-list-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,39 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+        //create a user to backend
+        const user = {
+            name: 'Matti Luukkainen',
+            username: 'mluukkai',
+            password: 'salainen'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', user)
+
         cy.visit('http://localhost:3000')
     })
 
     it('Login form is shown by default', function() {
         cy.contains('log in to application')
+    })
+
+    describe('Login', function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('salainen')
+            cy.get('#login-button').click()
+
+            cy.contains('Matti Luukkainen logged in')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('wrong')
+            cy.get('#login-button').click()
+
+            cy.get('.notification.error')
+              .should('contain', 'invalid username or password')
+              .and('have.css', 'color', 'rgb(255, 0, 0)')
+        })
     })
 })
--- PART4_and_PART5/blog-list-frontend/src/components/LoginForm.js
+++ PART4_and_PART5/blog-list-frontend/src/components/LoginForm.js
@@ -9,22 +9,22 @@ const LoginForm = (props) => {
         <div>
             username
           <input
+            id="username"
             type="text"
             value={props.username}
-            name="Username"
             onChange={({ target }) => props.setUsername(target.value)}
           />
         </div>
         <div>
             password
           <input
+            id="password"
             type="password"
             value={props.password}
-            name="Password"
             onChange={({ target }) => props.setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   )
