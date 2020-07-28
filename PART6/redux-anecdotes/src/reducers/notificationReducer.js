daa3caec49d626176e28256d39ad0825b4da1a2f
Crystal Yun <crystal@internal.rpahero.com>
2020-07-28 23:30:25 +1000

    exercise 6.10

---
+++ PART6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+const notificationReducer = (state = 'placeholder', action) => {
+    console.log('action hit notification reducer')
+    console.log('print out action in notification reducer ', action)
+    switch (action.type) {
+        case 'DISPLAY_NOTIFICATION':
+            return action.message
+        default:
+            return state
+    }
+}
+
+export const notificationChange = (message) => {
+    return {
+        type: 'DISPLAY_NOTIFICATION',
+        message
+    }
+}
+
+export default notificationReducer
