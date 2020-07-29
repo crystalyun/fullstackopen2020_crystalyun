593ccb3d65f96a669c7381e7a6b338f63feae6fd
Crystal Yun <crystal@internal.rpahero.com>
2020-07-29 18:42:16 +1000

    exercise 6.12

--- PART6/redux-anecdotes/src/App.js
+++ PART6/redux-anecdotes/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
       <Notification />
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
@@ -15,3 +17,28 @@ const App = () => {
 }
 
 export default App
+
+/*
+state object schema
+
+{
+  anecdotes : [ {anecdote1}, {anecdote2}, {anecdote3} ], ; each object has props `content`, `id`, `votes`
+
+  notification : "placeholder",
+
+  filter : "placeholder2"
+}
+
+anecdotes
+ - has anecdotesReducer
+ - actions : `INCREMENT_VOTE`, `ADD_ANECDOTE`
+
+notification
+  - has notificationReducer
+  - actions : `DISPLAY_NOTIFICATION`, `REMOVE_NOTIFICATION`
+
+filter
+  - has filterReducer
+  - actions : `UPDATE_FILTER_MESSAGE`
+
+*/
--- PART6/redux-anecdotes/src/components/AnecdoteList.js
+++ PART6/redux-anecdotes/src/components/AnecdoteList.js
@@ -6,6 +6,7 @@ import { displayNotification } from '../reducers/notificationReducer'
 const AnecdoteList = () => {
     const dispatch = useDispatch()
     const anecdotes = useSelector(state => state.anecdotes)
+    const searchMessage = useSelector(state => state.filter)
 
     const vote = ({ id, content }) => {
         dispatch(addVote(id))
@@ -13,7 +14,11 @@ const AnecdoteList = () => {
     }
 
     const byVotes = (a1, a2) => a2.votes - a1.votes
-    const sortedAnecdotes = [ ...anecdotes ].sort(byVotes)
+
+    const filteredAnecdotes = anecdotes.filter(ele => {
+      return (ele.content).includes(searchMessage)
+    })
+    const sortedAnecdotes = [ ...filteredAnecdotes ].sort(byVotes)
 
     return (
         <>
