f04d9df2cea804353b2a4b0649635f892ff85481
Crystal Yun <crystal@internal.rpahero.com>
2020-07-30 20:53:39 +1000

    exercise 6.15

--- PART6/redux-anecdotes/src/App.js
+++ PART6/redux-anecdotes/src/App.js
@@ -3,18 +3,24 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import anecdoteService from './services/anecdotes'
 import { useDispatch } from 'react-redux'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
+
   useEffect(() => {
     console.log('render once')
-    anecdoteService
-      .getAll()
-      .then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
+
+
+  // useEffect(() => {
+  //   console.log('render once')
+  //   anecdoteService
+  //     .getAll()
+  //     .then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  // }, [dispatch])
 
   return (
     <div>
--- PART6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ PART6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const anecdoteReducer = (state = [], action) => {
   console.log('state now (anecdoteReducer): ', state)
   console.log('action (anecdoteReducer)', action)
@@ -38,10 +40,21 @@ export const addAnecdoteOf = (data) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+// before using redux-thunk
+// export const initializeAnecdotes = (anecdotes) => {
+//   return {
+//     type: 'INIT_ANECDOTES',
+//     data: anecdotes
+//   }
+// }
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES', 
+      data: anecdotes,
+    })
   }
 }
 
