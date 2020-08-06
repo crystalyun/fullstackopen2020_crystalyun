cabc49fe440db5b4e16054bcf4c0e9ce7ee41563
Crystal Yun <crystal@internal.rpahero.com>
2020-08-06 19:18:34 +1000

    exercise 7.8

--- PART7/ultimate-hooks/src/App.js
+++ PART7/ultimate-hooks/src/App.js
@@ -1,8 +1,7 @@
-  
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
+// useField is a custom hook for Form input fields.
 const useField = (type) => {
   const [value, setValue] = useState('')
 
@@ -17,13 +16,27 @@ const useField = (type) => {
   }
 }
 
+// useResource is a custom hook to communicate with app's backend server by making CURD API calls.
+// useResource hook can be used for both notes and phone numbers.
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    console.log('effect to fetch initial data from backend')
+    axios
+      .get(baseUrl)
+      .then(response => {
+        console.log('promise fulfilled. ', response.data)
+        setResources(response.data)
+      })
+  }, []);
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    console.log('hit the server right', response.data) // for notes, {content: "dude", id: 3}. for persons, {name: "feng bo shen", number: "1892773", id: 3}
+    // before returning, push new data to `resources` array
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
@@ -52,6 +65,8 @@ const App = () => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
   }
+
+  console.log('App rendered')
 
   return (
     <div>
